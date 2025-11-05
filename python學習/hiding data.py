e3ff64381c7ecfe3ccca86fb0dcd31f538bdc42c
Isaiah Zheng <zjz88060133@gmail.com>
2025-11-05 15:57:31 +0800

    這個是對數據隱藏的學習

---
+++ python學習/hiding data.py
@@ -0,0 +1,23 @@
+import math
+class Circle:
+    def __init__(self,r=1):
+        self.__r=r
+
+    def get_area(self):
+        return self.__r**2*math.pi
+
+
+
+
+
+
+
+# class Circle:
+#     def __init__(self,r=1):
+#         self.r=r
+# circle1=Circle(5)
+# print(circle1.r)
+
+
+
+
