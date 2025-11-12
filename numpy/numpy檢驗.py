f116bae66982a09ba02b4376d78d56596a5eadcc
Isaiah Zheng <zjz88060133@gmail.com>
2025-11-12 21:16:09 +0800

    我最近學的很少，要開始復習了，只能寫那些有點無聊的代碼

---
+++ numpy/numpy檢驗.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+print("NumPy 載入成功！版本:", np.__version__)
+
---
+++ python-learn/復習.py
@@ -0,0 +1,18 @@
+
+# tx=open("pytest.txt",'a')
+# tx.write("hhhh")
+# tx.write("\n")
+# tx.write("hhhh")
+# tx.close()
+
+# def fun():
+#     a=1
+#     b=2
+#     c=3
+#     return a,b,c
+# print(fun())
+
+a="hello world you"
+a.split()
+print(a.split())
+print(a.pop(0))
